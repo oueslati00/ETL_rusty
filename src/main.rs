ac6eee206d4531ac35ae4218859bef9d6f4fe6eb
brahim Oueslati <boueslati76@gmail.com>
2024-03-24 18:32:19 +0100

    adding basic config for postgress adapter

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,43 @@ use axum::{
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
+use tokio_postgres::{NoTls, Error};
 // the input to our `create_user` handler
+#[tokio::main]
+async fn main() -> Result<(), Error>{
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+      let (client, connection) =
+      tokio_postgres::connect("host=localhost user=postgres", NoTls).await?;
+      tokio::spawn(async move {
+      if let Err(e) = connection.await {
+          eprintln!("connection error: {}", e);
+      }
+  });
+  Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, World"
+}
+
+async fn create_user(
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+    (StatusCode::CREATED, Json(user))
+}
+
 #[derive(Deserialize)]
 struct CreateUser {
     username: String,
@@ -16,43 +52,3 @@ struct User {
     id: u64,
     username: String,
 }
-
-
-#[tokio::main]
-async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-
-    
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
-
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World Brahim!"
-}
-
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
